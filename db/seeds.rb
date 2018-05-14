2a7bf10958b7ef59c601765ad2135d2b0285449d
juancgb <juan.c.garcia.b@correounivalle.edu.co>
2018-05-14 01:47:30 -0500

    seeds, cors

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Creacion de roles
+roles = Role.create([
+    { name: 'Admin', description: 'Administrador del sistema', status: true }
+])
+
+# Creacion de usuarios
+users = User.create([
+    { name: 'Admin', nickname: 'Admin', email: 'admin@gestionproyectos.edu.co', password: '123456789' }
+])
+
+# Creacion de sedes
+offices = Office.create([
+    { name: 'Admin', status: true }
+])
+
+# Creacion de la relacion usuarios-sedes
+UserRole.create([
+    { user: users.first, office: offices.first }
+])
