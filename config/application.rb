93f9484cef2d2c8f53a1ba174a3aaccfd6b0d661
Julio Marin <julio.marin@novaip.com.co>
2018-05-15 18:13:16 -0500

    exponer los headers

--- config/application.rb
+++ config/application.rb
@@ -33,7 +33,10 @@ module SistemaGestionProyectosBackend
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
-        resource '*', :headers => :any, :methods => [:get, :post, :options]
+        resource '*',
+        :headers => :any,
+        :expose  => ['access-token', 'client', 'expiry', 'token-type', 'uid', 'Authorization'],
+        :methods => [:get, :post, :options]
       end
     end
   end
