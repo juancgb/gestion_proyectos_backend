e0dbf9dd7225d8b1a5d95138b8b28c0fa96a3a7e
Juan Manuel Rodriguez Arenas <juanmanuel2121@gmail.com>
2018-09-08 21:27:57 -0500

    correccion para heroku

--- app/serializers/office_career_serializer.rb
+++ app/serializers/office_career_serializer.rb
@@ -1,4 +1,4 @@
-class OfficeCareercareerSerializer < ActiveModel::Serializer
+class OfficeCareerSerializer < ActiveModel::Serializer
   attributes :office_id, :career_id, :office, :career
   belongs_to :office
   belongs_to :career
