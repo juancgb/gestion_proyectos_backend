312e4a582dd470c644ddd026994b7aac5f4d34ea
Juan Manuel Rodriguez Arenas <juanmanuel2121@gmail.com>
2018-09-08 20:17:11 -0500

    correccion al endpoint find_career_by_office y find_office_by_career

--- app/controllers/api/v1/office_careers_controller.rb
+++ app/controllers/api/v1/office_careers_controller.rb
@@ -1,7 +1,20 @@
 class Api::V1::OfficeCareersController < ApplicationController
   #before_action :authenticate_user!
   before_action :set_office_career, only: [:show, :update, :destroy]
-  before_action :office_index, only: [:find_career_by_office]
+  #   before_action :office_index, only: [:find_career_by_office]
+  before_action :office_career_params, only: [:find_career_by_office, :find_office_by_career]
+  #Funcion que muestra todos los carreras por sede
+  def find_career_by_office
+    @office_careers = OfficeCareer.find_career_by_office(params[:office_id])
+    json_response(@office_careers)
+  end
+
+  #Funcion que muestra todos las sedes que ofrecen una carrera
+  def find_office_by_career
+    @office_careers = OfficeCareer.find_office_by_career(params[:career_id])
+    json_response(@office_careers)
+  end
+
   #Funcion que crea una carrera en una sede
   def create
     @office_career = OfficeCareer.create!(office_career_params)
--- app/models/office_career.rb
+++ app/models/office_career.rb
@@ -1,11 +1,10 @@
 class OfficeCareer < ApplicationRecord
-    belongs_to :office
-    belongs_to :career
-    def self.find_career_by_office(office_id)
-        Career.where(office_id: office_id).includes(:office, :career)
-    end
-    def self.find_office_by_career(career_id)
-        Career.where(office_id: career_id).includes(:office, :career)
-    end
-
+  belongs_to :office
+  belongs_to :career
+  def self.find_career_by_office(office_id)
+    OfficeCareer.where(office_id: office_id).includes(:office, :career)
+  end
+  def self.find_office_by_career(career_id)
+    OfficeCareer.where(office_id: career_id).includes(:office, :career)
+  end
 end
